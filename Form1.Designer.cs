6659c87e27072745467c54350f589c1800c4fdff
Alejandro Ocampo Cisneros <ocampoalejandro.oc@gmail.com>
2020-12-29 12:03:28 -0600

    Se agrega biblioteca de emgu y se programa la reproducción de videos

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,11 +34,13 @@
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
             this.btnDetect = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureFrameVideo = new System.Windows.Forms.PictureBox();
             this.menuStrip1.SuspendLayout();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureFrameVideo)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -64,22 +66,25 @@
             // importarVideoToolStripMenuItem
             // 
             this.importarVideoToolStripMenuItem.Name = "importarVideoToolStripMenuItem";
-            this.importarVideoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.importarVideoToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.importarVideoToolStripMenuItem.Text = "Importar video";
+            this.importarVideoToolStripMenuItem.Click += new System.EventHandler(this.importarVideoToolStripMenuItem_Click);
             // 
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
             // 
             // salirToolStripMenuItem
             // 
             this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
-            this.salirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.salirToolStripMenuItem.Text = "Salir";
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.btnPause);
+            this.panel1.Controls.Add(this.btnPlay);
             this.panel1.Controls.Add(this.btnDetect);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.panel1.Location = new System.Drawing.Point(0, 429);
@@ -87,27 +92,55 @@
             this.panel1.Size = new System.Drawing.Size(780, 100);
             this.panel1.TabIndex = 1;
             // 
+            // btnPause
+            // 
+            this.btnPause.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnPause.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPause.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPause.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnPause.Location = new System.Drawing.Point(290, 25);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(90, 49);
+            this.btnPause.TabIndex = 3;
+            this.btnPause.Text = "Pausa";
+            this.btnPause.UseVisualStyleBackColor = false;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.btnPlay.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlay.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnPlay.Location = new System.Drawing.Point(194, 25);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(90, 49);
+            this.btnPlay.TabIndex = 1;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = false;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
             // btnDetect
             // 
-            this.btnDetect.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.btnDetect.BackColor = System.Drawing.Color.Purple;
             this.btnDetect.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnDetect.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnDetect.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.btnDetect.Location = new System.Drawing.Point(27, 20);
+            this.btnDetect.Location = new System.Drawing.Point(482, 25);
             this.btnDetect.Name = "btnDetect";
             this.btnDetect.Size = new System.Drawing.Size(90, 49);
             this.btnDetect.TabIndex = 0;
             this.btnDetect.Text = "Detectar";
             this.btnDetect.UseVisualStyleBackColor = false;
             // 
-            // pictureBox1
+            // pictureFrameVideo
             // 
-            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.pictureBox1.Location = new System.Drawing.Point(74, 58);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(615, 352);
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
+            this.pictureFrameVideo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.pictureFrameVideo.Location = new System.Drawing.Point(74, 58);
+            this.pictureFrameVideo.Name = "pictureFrameVideo";
+            this.pictureFrameVideo.Size = new System.Drawing.Size(615, 352);
+            this.pictureFrameVideo.TabIndex = 2;
+            this.pictureFrameVideo.TabStop = false;
             // 
             // Form1
             // 
@@ -115,7 +148,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(18)))));
             this.ClientSize = new System.Drawing.Size(780, 529);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureFrameVideo);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
@@ -124,7 +157,7 @@
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.panel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureFrameVideo)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -139,7 +172,9 @@
         private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button btnDetect;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureFrameVideo;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnPlay;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,79 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
 
 namespace Detector_de_cuerpos_en_movimiento
 {
     public partial class Form1 : Form
     {
+
+        VideoCapture video;
+        bool pausa = false;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void importarVideoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ImportacionVideo();
+        }
+
+        private void ImportacionVideo()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                video = new VideoCapture(ofd.FileName);
+                Mat matriz = new Mat();
+                video.Read(matriz);
+                pictureFrameVideo.Image = matriz.Bitmap;
+            }
+        }
+
+        private async void btnPlay_Click(object sender, EventArgs e)
+        {
+            pausa = false;
+            if(video==null)
+            {
+                ImportacionVideo();
+            }
+            try
+            {
+                while(!pausa)
+                {
+                    Mat matriz = new Mat();
+                    video.Read(matriz);
+                    //Si la matriz está llena
+                    if (!matriz.IsEmpty)
+                    {
+                        pictureFrameVideo.Image = matriz.Bitmap;
+                        double fps = video.GetCaptureProperty(Emgu.CV.CvEnum.CapProp.Fps);
+                        //await Task.Delay(1000/Convert.ToInt32( fps));
+                        await Task.Delay(1000 / 60);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                
+
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            pausa = true;
+        }
+
+        
     }
 }
