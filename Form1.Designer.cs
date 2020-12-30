2b4d22b1e0c3f8b11c98ae4493ec6a0a7aeb6d43
Alejandro Ocampo Cisneros <ocampoalejandro.oc@gmail.com>
2020-12-30 03:32:32 -0600

    Se detecta movimiento en videos de baja calidad y con un algoritmo que requiere de mejoras

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,16 +34,31 @@
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
             this.btnPause = new System.Windows.Forms.Button();
             this.btnPlay = new System.Windows.Forms.Button();
             this.pictureFrameVideo = new System.Windows.Forms.PictureBox();
-            this.trackBar1 = new System.Windows.Forms.TrackBar();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericErosion = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.numericDilatacion = new System.Windows.Forms.NumericUpDown();
+            this.labeltama = new System.Windows.Forms.Label();
+            this.numericTama = new System.Windows.Forms.NumericUpDown();
             this.menuStrip1.SuspendLayout();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureFrameVideo)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureFrameVideo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericErosion)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericDilatacion)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericTama)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -52,7 +67,7 @@
             this.archivoToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(780, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(656, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -86,6 +101,16 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.numericTama);
+            this.panel1.Controls.Add(this.labeltama);
+            this.panel1.Controls.Add(this.numericDilatacion);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.numericErosion);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.numericUpDown2);
+            this.panel1.Controls.Add(this.numericUpDown1);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.trackBar1);
@@ -94,8 +119,39 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.panel1.Location = new System.Drawing.Point(0, 416);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(780, 152);
+            this.panel1.Size = new System.Drawing.Size(656, 152);
             this.panel1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(218, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "60";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(22, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "FPS";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 95);
+            this.trackBar1.Maximum = 180;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(210, 45);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.Value = 60;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
             // 
             // btnPause
             // 
@@ -128,49 +184,158 @@
             // pictureFrameVideo
             // 
             this.pictureFrameVideo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.pictureFrameVideo.Location = new System.Drawing.Point(74, 58);
+            this.pictureFrameVideo.Location = new System.Drawing.Point(12, 58);
             this.pictureFrameVideo.Name = "pictureFrameVideo";
             this.pictureFrameVideo.Size = new System.Drawing.Size(615, 352);
+            this.pictureFrameVideo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureFrameVideo.TabIndex = 2;
             this.pictureFrameVideo.TabStop = false;
             // 
-            // trackBar1
+            // numericUpDown1
             // 
-            this.trackBar1.Location = new System.Drawing.Point(12, 95);
-            this.trackBar1.Maximum = 100;
-            this.trackBar1.Minimum = 1;
-            this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(210, 45);
-            this.trackBar1.TabIndex = 4;
-            this.trackBar1.Value = 60;
-            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            this.numericUpDown1.Location = new System.Drawing.Point(534, 110);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(48, 20);
+            this.numericUpDown1.TabIndex = 7;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
             // 
-            // label1
+            // numericUpDown2
             // 
-            this.label1.AutoSize = true;
-            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(22, 79);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(27, 13);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "FPS";
+            this.numericUpDown2.Location = new System.Drawing.Point(588, 110);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(48, 20);
+            this.numericUpDown2.TabIndex = 8;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
             // 
-            // label2
+            // label3
             // 
-            this.label2.AutoSize = true;
-            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label2.Location = new System.Drawing.Point(218, 105);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(19, 13);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "60";
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(455, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Binarización";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(626, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Location = new System.Drawing.Point(306, 86);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Erosión";
+            // 
+            // numericErosion
+            // 
+            this.numericErosion.Location = new System.Drawing.Point(376, 84);
+            this.numericErosion.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericErosion.Name = "numericErosion";
+            this.numericErosion.Size = new System.Drawing.Size(48, 20);
+            this.numericErosion.TabIndex = 12;
+            this.numericErosion.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label6.Location = new System.Drawing.Point(306, 112);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Dilatación";
+            // 
+            // numericDilatacion
+            // 
+            this.numericDilatacion.Location = new System.Drawing.Point(376, 112);
+            this.numericDilatacion.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericDilatacion.Name = "numericDilatacion";
+            this.numericDilatacion.Size = new System.Drawing.Size(48, 20);
+            this.numericDilatacion.TabIndex = 14;
+            this.numericDilatacion.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // labeltama
+            // 
+            this.labeltama.AutoSize = true;
+            this.labeltama.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labeltama.Location = new System.Drawing.Point(455, 86);
+            this.labeltama.Name = "labeltama";
+            this.labeltama.Size = new System.Drawing.Size(46, 13);
+            this.labeltama.TabIndex = 15;
+            this.labeltama.Text = "Tamaño";
+            // 
+            // numericTama
+            // 
+            this.numericTama.Location = new System.Drawing.Point(534, 84);
+            this.numericTama.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericTama.Name = "numericTama";
+            this.numericTama.Size = new System.Drawing.Size(48, 20);
+            this.numericTama.TabIndex = 16;
+            this.numericTama.Value = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(18)))));
-            this.ClientSize = new System.Drawing.Size(780, 568);
+            this.ClientSize = new System.Drawing.Size(656, 568);
             this.Controls.Add(this.pictureFrameVideo);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.menuStrip1);
@@ -181,8 +346,13 @@
             this.menuStrip1.PerformLayout();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureFrameVideo)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureFrameVideo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericErosion)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericDilatacion)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericTama)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -202,6 +372,16 @@
         private System.Windows.Forms.TrackBar trackBar1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericTama;
+        private System.Windows.Forms.Label labeltama;
+        private System.Windows.Forms.NumericUpDown numericDilatacion;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown numericErosion;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -23,9 +23,10 @@ namespace Detector_de_cuerpos_en_movimiento
 
 
         //Region de detección de movimiento
+        Image<Bgr, byte> frame;
         Image<Gray, byte> frame1;
         Image<Gray, byte> frame2;
-        Image<Gray, byte> imgOut;
+        //Image<Gray, byte> imgOut;
 
         Graphics papel;
         Pen pluma = new Pen(Color.Red);
@@ -52,9 +53,11 @@ namespace Detector_de_cuerpos_en_movimiento
             if (ofd.ShowDialog() == DialogResult.OK)
             {
                 video = new VideoCapture(ofd.FileName);
-                Mat matriz = new Mat();
+                Mat matriz = new Mat();               
                 video.Read(matriz);
-                pictureFrameVideo.Image = matriz.Bitmap;
+                //Image<Bgr, byte> img = matriz.ToImage<Bgr, byte>();
+                //img = img.Resize(pictureFrameVideo.Width, pictureFrameVideo.Height, Emgu.CV.CvEnum.Inter.Linear);
+                //pictureFrameVideo.Image = matriz.Bitmap;
             }
         }
         #endregion
@@ -69,20 +72,58 @@ namespace Detector_de_cuerpos_en_movimiento
             }
             try
             {
+            
                 while(!pausa)
                 {
-                    Mat matriz = new Mat();
-                    video.Read(matriz);
+                    Mat mFrame2 = new Mat();
+                    Mat mFrame1 = new Mat();
+
+                    
+                    
+                    video.Retrieve(mFrame1);
+                video.Grab();
+                video.Grab();
+                video.Grab();
+                video.Grab();
+                //video.Grab();
+                //video.Grab();
+                //video.Grab();
+
+
+
+                video.Read(mFrame2);
+
+
+                frame = mFrame2.ToImage<Bgr,byte>();
+                    frame = frame.Resize(pictureFrameVideo.Width,pictureFrameVideo.Height,Emgu.CV.CvEnum.Inter.Lanczos4);
+                    
+                    frame1 = mFrame1.ToImage<Gray, byte>();
+                //frame1 = frame1.Resize(pictureFrameVideo.Width, pictureFrameVideo.Height, Emgu.CV.CvEnum.Inter.Lanczos4);
+                //await Task.Delay(1000);
+                frame2 = mFrame2.ToImage<Gray, byte>();
+                    //frame2 = frame2.Resize(pictureFrameVideo.Width, pictureFrameVideo.Height, Emgu.CV.CvEnum.Inter.Lanczos4);
+                        
+
+                    DibujaBlobs(GeneraImagenBlob(frame2,frame1));
+                    //pictureFrameVideo.Image = GeneraImagenBlob(frame2,frame1).Bitmap;
+
+
                     //Si la matriz está llena
-                    if (!matriz.IsEmpty)
+                    if (!mFrame1.IsEmpty)
                     {
-                        pictureFrameVideo.Image = matriz.Bitmap;
-                        //double fps = video.GetCaptureProperty(Emgu.CV.CvEnum.CapProp.Fps);
-                        //await Task.Delay(1000/Convert.ToInt32( fps));
+                    
+                    pictureFrameVideo.Image = frame.ToBitmap();
+                    //pictureFrameVideo.Image = GeneraImagenBlob(frame2, frame1).ToBitmap();
+                    //    pictureFrameVideo.Image = mFrame2.Bitmap;
+                    //    //pictureFrameVideo.Image = mFrame2.Bitmap;
+                    //    //double fps = video.GetCaptureProperty(Emgu.CV.CvEnum.CapProp.Fps);
+                    //    //await Task.Delay(1000/Convert.ToInt32( fps));
                         await Task.Delay(1000 / tiempo);
+                    
                     }
                     else
                     {
+                        video.Stop();
                         break;
                     }
                 }
@@ -90,7 +131,7 @@ namespace Detector_de_cuerpos_en_movimiento
 
             }catch(Exception ex)
             {
-                MessageBox.Show(ex.Message);
+                //MessageBox.Show(ex.Message);
             }
         }
 
@@ -102,6 +143,69 @@ namespace Detector_de_cuerpos_en_movimiento
 
         #endregion
 
+        #region GeneraImgBlob
+        private Image<Gray,byte> GeneraImagenBlob(Image<Gray,byte> frameActual, Image<Gray,byte> frameAnterior)
+        {
+            Image<Gray, byte> imgOut = new Image<Gray, byte>(frameActual.Width,frameActual.Height);
+            
+            for(int i=0;i<frameActual.Height;i++)
+            {
+                for(int j=0;j<frameActual.Width;j++)
+                {
+                    imgOut[i, j] = new Gray(diferenciaPixel(frameAnterior[i, j].Intensity, frameActual[i, j].Intensity));
+                }
+            }
+            imgOut = imgOut.ThresholdBinary(new Gray((int)numericUpDown1.Value), new Gray((int)numericUpDown2.Value));
+            imgOut.Erode((int)numericErosion.Value);
+            imgOut.Dilate((int)numericDilatacion.Value);
+            imgOut.Resize(pictureFrameVideo.Width,pictureFrameVideo.Height, Emgu.CV.CvEnum.Inter.Lanczos4);
+            return imgOut;    
+        }
+        #endregion
+
+        #region DiferenciarPixel
+        private double diferenciaPixel(double fondo, double frame)
+        {
+            double dif = 0;
+            dif = fondo - frame;
+            if (dif < 0)
+                dif = 0;
+            if (dif > 255)
+                dif = 255;
+            return dif;
+        }
+
+        #endregion
+
+        #region DibujaBlobs
+        private void DibujaBlobs(Image<Gray, byte> imgBlob)
+        {
+
+            Emgu.CV.Util.VectorOfVectorOfPoint contours = new Emgu.CV.Util.VectorOfVectorOfPoint();
+            Mat mat = new Mat();
+            imgBlob = imgBlob.Resize(pictureFrameVideo.Width, pictureFrameVideo.Height, Emgu.CV.CvEnum.Inter.Lanczos4);
+            CvInvoke.FindContours(imgBlob, contours, mat, Emgu.CV.CvEnum.RetrType.External, Emgu.CV.CvEnum.ChainApproxMethod.ChainApproxSimple);
+            for (int i = 0; i < contours.Size; i++)
+            {
+
+                var area = CvInvoke.ContourArea(contours[i]);
+                if (area > (int)numericTama.Value)
+                {
+                    Rectangle rect = CvInvoke.BoundingRectangle(contours[i]);
+                    CvInvoke.Rectangle(frame, rect, new MCvScalar(0, 0, 255), 2);
+
+                    //papel = pictureFrameVideo.CreateGraphics();
+                    //pluma.Width = 5;
+                    //pluma.Color = Color.DarkBlue;
+                    //papel.DrawRectangle(pluma, rect);
+                }
+
+            }
+
+
+
+        }
+        #endregion
 
         #region Tiempo
         private void trackBar1_ValueChanged(object sender, EventArgs e)
